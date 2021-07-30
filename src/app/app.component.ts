a8812391b86837edfc7624356eecff4ac9524e81
Ruslan <badalov2605@gmail.com>
2021-07-30 17:05:46 +0300

    initial commit v1.0

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  bannerImages = [
+    'assets/images/img1.jpg',
+    'assets/images/img2.jpg',
+    'assets/images/img3.jpg'
+    
+  ];
+}
