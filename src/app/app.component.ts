430e9fd1f3639315e24926b3eb053a920e48ab4c
Max <maxchabanov2009@gmail.com>
2021-07-30 18:00:00 +0300

    stable version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,12 @@ export class AppComponent {
   bannerImages = [
     'assets/images/img1.jpg',
     'assets/images/img2.jpg',
-    'assets/images/img3.jpg'
-    
+    'assets/images/img3.jpg',
+    'assets/images/img3.jpg',
+  ];
+  bannerImages3 = [
+    'assets/images/img1.jpg',
+    'assets/images/img2.jpg',
+    'assets/images/img3.jpg',
   ];
 }
