d15c2794238fb9d22a63dc66b91fdb736d48d70d
Max <maxchabanov2009@gmail.com>
2021-08-02 17:33:39 +0300

    changed mobile version click

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,11 +1,11 @@
-import {Component, HostListener, Input} from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css'],
 })
-export class BannerComponent{
+export class BannerComponent {
   @Input() images: string[] = [];
 
   windowWidth = window.innerWidth;
@@ -17,10 +17,10 @@ export class BannerComponent{
     this.windowWidth = event.target.innerWidth;
   }
 
-  bannerClick(index: number, event: any) {
-    this.activeImage = index;
-
+  bannerClick(event: any) {
     if (event.target.className.includes('opened')) {
+      window.location.href = 'https://www.google.com';
+    } else if (this.windowWidth < 700) {
       window.location.href = 'https://www.google.com';
     }
   }
